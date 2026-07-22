97ca96a672c9e4a9260e92471eb36aebadbd498c
saicharanisigntech-svg <saicharan.isigntech@gmail.com>
2026-07-22 14:45:43 +0530

    Create React project using Vite

---
+++ src/attendance/attendance.js
@@ -0,0 +1,3 @@
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("Attendance Module Loaded Successfully");
+});
---
+++ src/departments/departments.js
@@ -0,0 +1,3 @@
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("Departments Module Loaded Successfully");
+});
